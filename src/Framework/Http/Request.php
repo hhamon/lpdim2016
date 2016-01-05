042c7c49a3b86c66daa76940b64f3c0a2f85df95
Hugo Hamon <hugo.hamon@sensiolabs.com>
2016-01-05 16:59:50 +0100

    [Framework] refactored Request::setHeaders method.

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -136,16 +136,7 @@ class Request
     private function setHeaders(array $headers)
     {
         foreach ($headers as $header => $value) {
-            $header = strtolower($header);
-
-            if (isset($this->headers[$header])) {
-                throw new \RuntimeException(sprintf(
-                    'Header %s is already defined and cannot be set twice.',
-                    $header
-                ));
-            }
-
-            $this->headers[$header] = $value;
+            $this->addHeader($header, $value);
         }
     }
 
@@ -154,5 +145,27 @@ class Request
         $name = strtolower($name);
 
         return isset($this->headers[$name]) ? $this->headers[$name] : null;
+    }
+
+    /**
+     * Adds a new normalized header value to the list of all headers.
+     *
+     * @param string $header The HTTP header name
+     * @param string $value  The HTTP header value
+     *
+     * @throws \RuntimeException
+     */
+    private function addHeader($header, $value)
+    {
+        $header = strtolower($header);
+
+        if (isset($this->headers[$header])) {
+            throw new \RuntimeException(sprintf(
+                'Header %s is already defined and cannot be set twice.',
+                $header
+            ));
+        }
+
+        $this->headers[$header] = $value;
     }
 }
