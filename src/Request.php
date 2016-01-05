19aa811e234c1ac4114398b870285c0ed53a8884
Hugo Hamon <hugo.hamon@sensiolabs.com>
2016-01-05 12:32:30 +0100

    Started foundations of the PHP framework.

---
+++ src/Request.php
@@ -0,0 +1,73 @@
+<?php
+
+class Request
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const PATCH = 'PATCH';
+    const OPTIONS = 'OPTIONS';
+    const TRACE = 'TRACE';
+    const HEAD = 'HEAD';
+    const DELETE = 'DELETE';
+
+    const HTTP = 'HTTP';
+    const HTTPS = 'HTTPS';
+
+    private $method;
+    private $scheme;
+    private $schemeVersion;
+    private $path;
+    private $headers;
+    private $body;
+
+    /**
+     * Constructor.
+     *
+     * @param string $method        The HTTP verb
+     * @param string $path          The resource path on the server
+     * @param string $scheme        The protocole name (HTTP or HTTPS)
+     * @param string $schemeVersion The scheme version (ie: 1.0, 1.1 or 2.0)
+     * @param array  $headers       An associative array of headers
+     * @param string $body          The request content
+     */
+    public function __construct($method, $path, $scheme, $schemeVersion, array $headers = [], $body = '')
+    {
+        $this->method = $method;
+        $this->path = $path;
+        $this->scheme = $scheme;
+        $this->schemeVersion = $schemeVersion;
+        $this->headers = $headers;
+        $this->body = $body;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getScheme()
+    {
+        return $this->scheme;
+    }
+
+    public function getSchemeVersion()
+    {
+        return $this->schemeVersion;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
---
+++ tests/RequestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateRequestInstance()
+    {
+        $request = new Request(Request::GET, '/', Request::HTTP, '1.1');
+
+        $this->assertSame(Request::GET, $request->getMethod());
+        $this->assertSame('/', $request->getPath());
+        $this->assertSame(Request::HTTP, $request->getScheme());
+        $this->assertSame('1.1', $request->getSchemeVersion());
+        $this->assertEmpty($request->getHeaders());
+        $this->assertEmpty($request->getBody());
+    }
+}
